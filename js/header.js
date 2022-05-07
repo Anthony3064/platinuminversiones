1f015565bbe2e5b2a1a08049c15052e85c110ae6
Anthony Flores Boza <anthony3064@gmail.com>
2022-05-06 19:38:28 -0600

    Change @medias to logo

--- js/header.js
+++ js/header.js
@@ -1,9 +1,20 @@
 
-function changeClass(elementToActive) {
-    
-    let currentActive = document.getElementsByClassName("active");
-    currentActive[0].classList.remove("active");
-
-    elementToActive.classList.add("active");
-    
+const changeClass = (elementToActive) => {
+    let currentActive = document.getElementsByClassName("nav-menu-link_active");
+    currentActive[0].classList.remove("nav-menu-link_active");
+    elementToActive.classList.add("nav-menu-link_active");
 }
+
+const navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".nav-menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu_visible");
+
+    if (navMenu.classList.contains("aria-label")) {
+        navToggle.setAttribute("aria-label", "Cerrar menú");
+    } else {
+        navToggle.setAttribute("aria-label", "Abrir menú");
+    }
+
+});
