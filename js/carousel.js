2de9c4b1617e88655d751b4fe580ca60061c862d
Anthony Flores Boza <anthony3064@gmail.com>
2022-05-13 00:49:25 -0600

    Add brokers section and change of style

--- js/carousel.js
+++ js/carousel.js
@@ -4,7 +4,7 @@ showSlides(slideIndex);
 let intervalTime;
 
 function repeat() {
-    intervalTime = setInterval(nextImage, 10000);
+    //intervalTime = setInterval(nextImage, 10000);
 }
 
 function nextImage() {
@@ -28,17 +28,12 @@ function showSlides(n) {
     setTimeout(() => {
         let i;
         let slides = document.getElementsByClassName("mySlides");
-        let dots = document.getElementsByClassName("dot");
         if (n > slides.length) { slideIndex = 1 }
         if (n < 1) { slideIndex = slides.length }
         for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-        }
         slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
     },290);
 
 }
